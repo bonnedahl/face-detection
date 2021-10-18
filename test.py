84cdbe0112f4b4b1a0a013e63fe68f1000accd20
Bonnedahl <62655732+bonnedahl@users.noreply.github.com>
2021-10-18 10:21:39 +0200

    Removed unnecessary code and commented a bit

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ with torch.no_grad():
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-        print(idx)
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
